af3d4bd08cc796a5a2918a493967e023d96cdaa5
Martin Kierkegaard <mark@easj.dk>
2016-10-26 19:48:53 +0200

    solution for the exercise

--- Sandbox/StudentInfo.cs
+++ Sandbox/StudentInfo.cs
@@ -17,30 +17,41 @@ namespace Sandbox
         // Return the number of students in the group of students
         public int GetStudentCount()
         {
-            // The below must be changed
-            return 0;
+            return students.Count;
         }
 
         // Add a single student to the group of students
         public void AddStudent(int id, Student aStudent)
         {
-            // Add code here
+            students.Add(id, aStudent);
         }
 
         // Given an id, return the student with that id.
         // If no student exists with the given id, return null
         public Student GetStudent(int id)
         {
-            // The below must be changed
-            return null;
+            if (students.ContainsKey(id))
+            {
+                return students[id];
+            }
+            else
+            {
+                return null;
+            }
         }
 
         // Given an id, return the score average for the student with that id.
         // If no student exists with the given id, return 0 (zero).
         public int GetAverageForStudent(int id)
         {
-            // The below must be changed
-            return 0;
+            if (students.ContainsKey(id))
+            {
+                return students[id].GetScoreAverage();
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         // Calculate the total test score average for ALL students
@@ -48,8 +59,15 @@ namespace Sandbox
         // TIP: Use the method GetAllStudentId and a loop...
         public int GetTotalAverage()
         {
-            // The below must be changed
-            return 0;
+            List<int> idList = GetAllStudentId();
+            int sum = 0;
+
+            foreach (int id in idList)
+            {
+                sum = sum + GetAverageForStudent(id);
+            }
+
+            return (sum / idList.Count);
         }
 
         // Returns a list of all ids of the students in the group of students
